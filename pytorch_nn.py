2aea2019536f86e8ba80a55e9a3cfdf2c0bde58d
Glebys Gonzalez <glebysg@gmail.com>
2019-05-08 00:39:38 -0400

    WHUYYY JUST WHYYY

--- pytorch_nn.py
+++ pytorch_nn.py
@@ -4,6 +4,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 
 def simple_gradient():
@@ -15,35 +18,39 @@ def simple_gradient():
     print(x.grad)
 
 
-def create_nn(batch_size=200, learning_rate=0.01, epochs=10,
+def create_nn(batch_size=20, learning_rate=0.01, epochs=10,
               log_interval=10):
+    # Load Data
+    x_full,y_full = load_boston(return_X_y=True)
+    # Split the data for testing
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_full, y_full, test_size=0.3, random_state=42)
+    y_train = y_train.reshape((-1,1))
+    y_test = y_test.reshape((-1,1))
+    # Normalization and preprocessing here
 
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+    # Make the data into tensors
+    train_tensor = torch.tensor(np.concatenate((x_train,y_train), axis=1)).float()
+    test_tensor = torch.tensor(np.concatenate((x_test,y_test), axis=1)).float()
+
+    # Normalization and preprocessing hnto the dataset manager
+    train_loader = torch.utils.data.DataLoader(train_tensor,
         batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])),
+    test_loader = torch.utils.data.DataLoader(test_tensor,
         batch_size=batch_size, shuffle=True)
 
     class Net(nn.Module):
         def __init__(self):
             super(Net, self).__init__()
-            self.fc1 = nn.Linear(28 * 28, 200)
-            self.fc2 = nn.Linear(200, 200)
-            self.fc3 = nn.Linear(200, 10)
+            self.fc1 = nn.Linear(13, 20)
+            self.fc2 = nn.Linear(20, 20)
+            self.fc3 = nn.Linear(20, 1)
 
         def forward(self, x):
             x = F.relu(self.fc1(x))
             x = F.relu(self.fc2(x))
             x = self.fc3(x)
-            return F.log_softmax(x, dim=0)
+            return x
 
     net = Net()
     print(net)
@@ -51,17 +58,17 @@ def create_nn(batch_size=200, learning_rate=0.01, epochs=10,
     # create a stochastic gradient descent optimizer
     optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
     # create a loss function
-    criterion = nn.NLLLoss()
+    criterion = nn.MSELoss()
 
     # run the main training loop
     for epoch in range(epochs):
-        for batch_idx, (data, target) in enumerate(train_loader):
-            data, target = Variable(data), Variable(target)
-            # resize data from (batch_size, 1, 28, 28) to (batch_size, 28*28)
-            data = data.view(-1, 28*28)
+        for batch_idx, full_data in enumerate(train_loader):
+            data, target = Variable(full_data[:,0:13]), Variable(full_data[:,13])
+            data = data.view(-1, 13)
             optimizer.zero_grad()
             net_out = net(data)
-            loss = criterion(net_out, target)
+            print "TARGET:", target.view(20,1)
+            loss = criterion(net_out, target.view(20,1))
             loss.backward()
             optimizer.step()
             if batch_idx % log_interval == 0:
@@ -72,13 +79,17 @@ def create_nn(batch_size=200, learning_rate=0.01, epochs=10,
     # run a test loop
     test_loss = 0
     correct = 0
+    epsilon = 0.1
     for data, target in test_loader:
         data, target = Variable(data, requires_grad=False), Variable(target)
-        data = data.view(-1, 28 * 28)
+        data = data.view(-1, 13)
         net_out = net(data)
         # sum up batch loss
-        test_loss += criterion(net_out, target).data.item()
-        pred = net_out.data.max(1)[1]  # get the index of the max log-probability
+        print "TARGET:", target.view(20,1)
+        test_loss += criterion(net_out, target.view(20,1))
+        # if the difference is less than the threshold, consider it an acurate response.
+        # else, consider it a failed example
+        pred = 1 if abs(target.data.item()-net_out.data.item()) < epsilon else 0
         correct += pred.eq(target.data).sum()
 
     test_loss /= len(test_loader.dataset)
